9f034cfa19ea6fdc488abb8a91bc268d5cb006c9
Fan Jin <im.fan.jin@gmail.com>
2016-06-27 14:48:59 -0400

    Add queue implementation

---
+++ queue/queue.rb
@@ -0,0 +1,36 @@
+# n size queue implemented using array
+class Queue
+
+  private
+
+  attr_accessor :queue, :head, :tail
+
+  public
+
+  def initialize(size)
+    self.queue = Array.new size + 1
+    self.head = self.tail = 0
+  end
+
+  def enqueue(element)
+    raise "queue overflow" if full?
+    queue[tail] = element
+    self.tail = (tail + 1) % queue.length
+    true
+  end
+
+  def dequeue
+    raise "queue underflow" if empty?
+    self.head = (head + 1) % queue.length
+    queue[head - 1]
+  end
+
+  def empty?
+    head == tail
+  end
+
+  def full?
+    (tail + 1) % queue.length == head
+  end
+
+end
